5609df2257fab10696e47a00458585694b614ee0
xuliu15 <x.liu.61@student.rug.nl>
2024-04-10 18:06:13 +0200

    submit a reading

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, Path, HTTPException, status
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+app = FastAPI()
+
+
+class Reading(BaseModel):
+    number: int
+    time: datetime
+    #factorial: Optional[int] = None
+
+    
+factorial_db = {}
+
+@app.get("/")
+def root_endpoint():
+    return print('This is the root endpoint.')
+
+@app.get("/get-factorial-by-id/{reading_id}/")
+def get_factorial(reading_id:int = Path(..., description="The factorial of a reading you'd like to know.", gt=0, lt=20)):
+    return factorial_db[reading_id]
+
+@app.get("/get-factorial-by-reading/")
+def get_factorial(number:int = Path(..., description = "The factorial of a reading you'd like to know.", gt=0)):
+    for reading_id in factorial_db:
+        if factorial_db[reading_id].number == number:
+            return factorial_db[reading_id]
+            #return factorial_calculation(factorial_db[reading_id].number)
+    raise HTTPException(status_code=404, detail='reading not found')
+
+@app.post("/submit-reading/reading_id/")
+def submit_reading(reading_id: int, reading: Reading):
+    if reading_id in factorial_db:
+        raise HTTPException(status_code=400, detail='reading ID already exists')
+    factorial_db[reading_id] = reading
+    return factorial_db[reading_id]
+
+def factorial_calculation(reading: Reading):
+    factorial = 1
+    if reading.number < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    elif reading.number == 0:
+        factorial = 1 
+    else:
+        for i in range(1, reading.number):
+            factorial *= i
+        return factorial
+
